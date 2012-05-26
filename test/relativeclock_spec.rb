3b1f18ab1bd5b4bc29cbbc4612d6e5cc2dc14f70
airmailster <airmailster@gmail.com>
2012-05-26 15:31:12 +0800

    Update test/relativeclock_spec.rb

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -7,11 +7,25 @@ describe SingaporeRelativeClock do
 
 	it "should..." do
               @clock.offsetFor("Delhi").should_not be_nil
-	end
-	
-	
-	it "should..." do
-              @clock.offsetFor("Delhi").should be equal 2.5              
+              @clock.offsetFor("Delhi").should_be equal 2.5              
+              @clock.offsetFor("Singapore").should_not be_nil
+              @clock.offsetFor("Singapore").should_be equal 0
 	end
 
+	it "should return -2 for Sydney" do
+		@clock.timeDifferenceTo "Sydney" should equal -2
+	end
+	
+	it "should return 0 for singapore" do
+	       @clock.timeDifferenceTo("Singapore") === 0
+	end
+	
+	def timeDifferenceTo (city)
+		if (city equal "Singapore") do 
+		0 
+		end
+		if (city equal "Sydney") do
+		-3
+		end
+	end
 end
