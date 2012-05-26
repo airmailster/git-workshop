025280c2d1b10605cf719746bca1bdc6557bb9bf
airmailster <airmailster@gmail.com>
2012-05-26 14:44:31 +0800

    Update test/relativeclock_spec.rb

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -6,7 +6,12 @@ describe SingaporeRelativeClock do
 	end
 
 	it "should..." do
-
+              @clock.offsetFor("Delhi").should_not be_nil
+	end
+	
+	
+	it "should..." do
+              @clock.offsetFor("Delhi").should be equal 2.5              
 	end
 
 end
